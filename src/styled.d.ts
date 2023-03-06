10c24c3d48f8dc5943382cde4fa95d7b7f0d5dad
e6d1fe <e6d1fe@gmail.com>
2023-03-06 15:45:46 +0900

    tried out animations

---
+++ src/styled.d.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from "styled-components";
+
+// import original module declarations
+import "styled-components";
+
+// and extend them!
+declare module "styled-components" {
+  export interface DefaultTheme {}
+}
