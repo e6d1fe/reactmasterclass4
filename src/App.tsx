dd952087e98ec4a34461c3d553a096b8f72ca4d1
e6d1fe <e6d1fe@gmail.com>
2023-03-13 21:17:03 +0900

    studied AnimatePresence

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,60 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { motion, useMotionValue } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
 
 const Box = styled(motion.div)`
-  width: 180px;
-  height: 180px;
+  width: 400px;
+  height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
-const BiggerBox = styled(motion.div)`
-  width: 400px;
-  height: 400px;
-  background-color: rgba(255, 255, 255, 0.2);
-  border-radius: 40px;
   display: flex;
-  align-items: center;
   justify-content: center;
-  overflow: hidden;
+  align-items: center;
+  font-size: 28px;
+  position: absolute;
+  top: 250px;
 `;
 
 const boxVariants = {
-  hover: {
-    rotateZ: 90,
+  invisible: { x: 500, opacity: 0, scale: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.5,
+    },
   },
-  click: {
-    borderRadius: "100px",
-  },
+  exit: { x: -500, opacity: 0, scale: 0 },
 };
 
 function App() {
-  const xcoord = useMotionValue(0);
-  useEffect(() => {
-    xcoord.onChange(() => console.log(xcoord.get()));
-  }, [xcoord]);
-  console.log(xcoord);
+  const [visible, setVisible] = useState(1);
+  const nextPlease = () => setVisible((prev) => (prev === 10 ? 1 : prev + 1));
+  const prevPlease = () => setVisible((prev) => (prev === 1 ? 10 : prev - 1));
   return (
     <Wrapper>
-      <button onClick={() => xcoord.set(200)}>Click me!</button>
-      <Box style={{ x: xcoord }} drag="x" dragSnapToOrigin />
+      <AnimatePresence>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
+          i === visible ? (
+            <Box variants={boxVariants} initial="invisible" animate="visible" exit="exit" key={i}>
+              {i}
+            </Box>
+          ) : null
+        )}
+      </AnimatePresence>
+      <button onClick={nextPlease}>Next</button>
+      <button onClick={prevPlease}>Previous</button>
     </Wrapper>
   );
 }
